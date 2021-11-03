504a831315d6faed6e301ea1ab4e23a557713334
ashleve <zalewski.ukas@gmail.com>
2021-11-03 13:49:15 +0100

    Upload files

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+
+import torch
+
+from pytorch_lightning import LightningModule, Trainer
+from mnist_datamodule import MNISTDataModule
+from simple_dense_net import SimpleDenseNet
+from torchmetrics.classification.accuracy import Accuracy
+
+
+class MNISTModel(LightningModule):
+    def __init__(
+        self,
+        input_size: int = 784,
+        lin1_size: int = 256,
+        lin2_size: int = 256,
+        lin3_size: int = 256,
+        output_size: int = 10,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.model = SimpleDenseNet(hparams=self.hparams)
+
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        self.val_acc = Accuracy()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def step(self, batch):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        return loss, preds, y
+
+    def training_step(self, batch, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+        return loss
+
+    def validation_step(self, batch, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        acc = self.val_acc(preds, targets)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_epoch_end(self, outputs):
+        acc = self.val_acc.compute()  # get val accuracy from current epoch
+        self.log("val/acc_2", acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=0.001)
+
+
+def run():
+    datamodule = MNISTDataModule()
+
+    model = MNISTModel()
+    trainer = Trainer(
+        max_epochs=3,
+    )
+    trainer.fit(model=model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    run()
---
+++ mnist_datamodule.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+
+
+class MNISTDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters(logger=False)
+
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+
+    @property
+    def num_classes(self) -> int:
+        return 10
+
+    def prepare_data(self):
+        MNIST(self.hparams.data_dir, train=True, download=True)
+        MNIST(self.hparams.data_dir, train=False, download=True)
+
+    def setup(self, stage: Optional[str] = None):
+        self.data_train = MNIST(self.hparams.data_dir, train=True, transform=self.transforms)
+        self.data_val = MNIST(self.hparams.data_dir, train=False, transform=self.transforms)
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+            drop_last=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+            drop_last=True,
+        )
